8ad205f1a035031ad1e8779945c63ae2b91bb25a
MuhammadHafijur <hafijur.salah@gmail.com>
2021-11-04 08:53:05 +0600

    added styling for appointment section

--- src/Pages/AppoinmentBanner/AppointmentBanner.js
+++ src/Pages/AppoinmentBanner/AppointmentBanner.js
@@ -3,25 +3,41 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import doctor from '../../images/doctor.png'
 import bg from '../../images/appointment-bg.png'
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
-const appointmentBg = {
-    background: `url(${bg})`
+const appointmentBanner = {
+    background: `url(${bg})`,
+    backgroundColor: 'rgba(45, 58, 74, 0.9)',
+    backgroundBlendMode: 'darken, luminosity',
+    marginTop: 175
 }
 
 const AppointmentBanner = () => {
     return (
-        <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
+        <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <img 
-          style={{width: 400}}
+          style={{width: 400, marginTop: '-110px'}}
           src={doctor} alt="" />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h6">
+        <Grid item xs={12} md={6} sx={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            textAlign: 'left'}}>
+          <Box>
+          <Typography variant="h6" sx={{mb: 5}} style={{color: '#5CE7ED'}}>
               Appointment
           </Typography>
+          <Typography variant="h4" style={{color: 'white'}}>
+              Make an Appointment Today
+          </Typography>
+          <Typography variant="h6" sx={{my: 5}} style={{color: 'white', fontSize: '14', fontWight: '300'}}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum architecto omnis molestiae atque non nisi corporis consectetur accusamus earum quos.
+          </Typography>
+          <Button variant="contained" style={{backgroundColor: '#5CE7ED'}}>Learn More</Button>
+          </Box>
         </Grid>
         
       </Grid>
